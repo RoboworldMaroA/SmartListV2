0494b2805683293ae431f3fc0339ff767c02d393
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-04-28 18:32:28 +0100

    28 April 2022 18:32 new changes

--- smartList/src/components/EssentialList2.js
+++ smartList/src/components/EssentialList2.js
@@ -44,7 +44,31 @@ const EssentialList = ({}) => {
                         <span className="card-title">ESSENTIALS</span>
                     </div>
                     <div className="card-content">
-                        <form id="essential" action="#">
+
+                        <form id="documents"  action="#">
+                            <label>
+                                <div id="toRight" ><button onClick={(event)=>changeQuantityMinus(event)}>-</button><button> {underwearQty} </button><button onClick={(event)=>changeQuantityPlus(event)}>+</button> </div>
+                            </label>
+                            <p>
+                                <label>
+                                    <input id="crossLine" type="checkbox" className="filled-in" />
+                                    <span>Passport</span>
+                                    <div id="toRight" ><button onClick={(event)=>changeQuantityMinus(event)}>-</button><button> {underwearQty} </button><button onClick={(event)=>changeQuantityPlus(event)}>+</button> </div>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label>
+                                    <input  type="checkbox" className="filled-in"/>
+                                    <span>Identity card</span>
+                                </label>
+                            </p>
+
+
+
+
+
+                            {/*??*/}
 
                             <label>
                                 <div id="toRight">
@@ -56,7 +80,7 @@ const EssentialList = ({}) => {
                             <p>
                                 <label>
                                     <input id="crossLine" type="checkbox" className="filled-in"/>
-                                    <span>Passport</span>
+                                    <span>House keys</span>
                                     <div id="toRight">
                                         <button onClick={(event) => changeQuantityMinus(event)}>-</button>
                                         <button> {underwearQty} </button>
@@ -65,6 +89,74 @@ const EssentialList = ({}) => {
                                 </label>
                             </p>
 
+
+                            <p>
+                                <label>
+                                    <input id="crossLine" type="checkbox" className="filled-in"/>
+                                    <span>Cash</span>
+                                    <div id="toRight">
+                                        <button onClick={(event) => changeQuantityMinus(event)}>-</button>
+                                        <button> {underwearQty} </button>
+                                        <button onClick={(event) => changeQuantityPlus(event)}>+</button>
+                                    </div>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label>
+                                    <input id="crossLine" type="checkbox" className="filled-in"/>
+                                    <span>Hotel booking confirmation</span>
+                                    <div id="toRight">
+                                        <button onClick={(event) => changeQuantityMinus(event)}>-</button>
+                                        <button> {underwearQty} </button>
+                                        <button onClick={(event) => changeQuantityPlus(event)}>+</button>
+                                    </div>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label>
+                                    <input type="checkbox" className="filled-in"/>
+                                    <span> Plane Ticket </span>
+                                </label>
+                            </p>
+
+                            {/*???*/}
+
+
+                            {/*??*/}
+
+                            <label>
+                                <div id="toRight">
+                                    <button onClick={(event) => changeQuantityMinus(event)}>-</button>
+                                    <button> {underwearQty} </button>
+                                    <button onClick={(event) => changeQuantityPlus(event)}>+</button>
+                                </div>
+                            </label>
+                            <p>
+                                <label>
+                                    <input id="crossLine" type="checkbox" className="filled-in"/>
+                                    <span>Driving Licence</span>
+                                    <div id="toRight">
+                                        <button onClick={(event) => changeQuantityMinus(event)}>-</button>
+                                        <button> {underwearQty} </button>
+                                        <button onClick={(event) => changeQuantityPlus(event)}>+</button>
+                                    </div>
+                                </label>
+                            </p>
+
+
+                            <p>
+                                <label>
+                                    <input id="crossLine" type="checkbox" className="filled-in"/>
+                                    <span>Certificate of Vaccination for COVID 19</span>
+                                    <div id="toRight">
+                                        <button onClick={(event) => changeQuantityMinus(event)}>-</button>
+                                        <button> {underwearQty} </button>
+                                        <button onClick={(event) => changeQuantityPlus(event)}>+</button>
+                                    </div>
+                                </label>
+                            </p>
 
                             <p>
                                 <label>
@@ -80,24 +172,89 @@ const EssentialList = ({}) => {
 
                             <p>
                                 <label>
-                                    <input id="crossLine" type="checkbox" className="filled-in"/>
-                                    <span>Passport</span>
-                                    <div id="toRight">
-                                        <button onClick={(event) => changeQuantityMinus(event)}>-</button>
-                                        <button> {underwearQty} </button>
-                                        <button onClick={(event) => changeQuantityPlus(event)}>+</button>
-                                    </div>
-                                </label>
-                            </p>
-
-                            <p>
-                                <label>
                                     <input type="checkbox" className="filled-in"/>
-                                    <span>ID</span>
+                                    <span>Boarding Pass</span>
                                 </label>
                             </p>
 
+                            {/*???*/}
+
+
+
+
+
+
+                            {/*save list button subnit shoul execute function used to saving list on the computer*/}
+                            {/*<div className="row">*/}
+                            {/*    <Link to="../SaveList">*/}
+                            {/*        <button className="waves-effect waves-light btn #795548 brown " type="submit" name="action">*/}
+                            {/*            Save List(Saving not ready yet)*/}
+                            {/*        </button>*/}
+                            {/*    </Link>*/}
+                            {/*</div>*/}
                         </form>
+
+
+
+
+
+
+
+
+                        {/*<form id="essential" action="#">*/}
+
+                        {/*    <label>*/}
+                        {/*        <div id="toRight">*/}
+                        {/*            <button onClick={(event) => changeQuantityMinus(event)}>-</button>*/}
+                        {/*            <button> {underwearQty} </button>*/}
+                        {/*            <button onClick={(event) => changeQuantityPlus(event)}>+</button>*/}
+                        {/*        </div>*/}
+                        {/*    </label>*/}
+                        {/*    <p>*/}
+                        {/*        <label>*/}
+                        {/*            <input id="crossLine" type="checkbox" className="filled-in"/>*/}
+                        {/*            <span>Passport</span>*/}
+                        {/*            <div id="toRight">*/}
+                        {/*                <button onClick={(event) => changeQuantityMinus(event)}>-</button>*/}
+                        {/*                <button> {underwearQty} </button>*/}
+                        {/*                <button onClick={(event) => changeQuantityPlus(event)}>+</button>*/}
+                        {/*            </div>*/}
+                        {/*        </label>*/}
+                        {/*    </p>*/}
+
+
+                        {/*    <p>*/}
+                        {/*        <label>*/}
+                        {/*            <input id="crossLine" type="checkbox" className="filled-in"/>*/}
+                        {/*            <span>Travel Insurance</span>*/}
+                        {/*            <div id="toRight">*/}
+                        {/*                <button onClick={(event) => changeQuantityMinus(event)}>-</button>*/}
+                        {/*                <button> {underwearQty} </button>*/}
+                        {/*                <button onClick={(event) => changeQuantityPlus(event)}>+</button>*/}
+                        {/*            </div>*/}
+                        {/*        </label>*/}
+                        {/*    </p>*/}
+
+                        {/*    <p>*/}
+                        {/*        <label>*/}
+                        {/*            <input id="crossLine" type="checkbox" className="filled-in"/>*/}
+                        {/*            <span>Passport</span>*/}
+                        {/*            <div id="toRight">*/}
+                        {/*                <button onClick={(event) => changeQuantityMinus(event)}>-</button>*/}
+                        {/*                <button> {underwearQty} </button>*/}
+                        {/*                <button onClick={(event) => changeQuantityPlus(event)}>+</button>*/}
+                        {/*            </div>*/}
+                        {/*        </label>*/}
+                        {/*    </p>*/}
+
+                        {/*    <p>*/}
+                        {/*        <label>*/}
+                        {/*            <input type="checkbox" className="filled-in"/>*/}
+                        {/*            <span>ID</span>*/}
+                        {/*        </label>*/}
+                        {/*    </p>*/}
+
+                        {/*</form>*/}
 
                     </div>
                 </div>
